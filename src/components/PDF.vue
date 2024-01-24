519d0f1225ef3343fd192e4c4a3ea1ccab430411
nlply <yangliu.nlp@gmail.com>
2024-01-24 21:41:33 +0800

    commit

---
+++ src/components/PDF.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="pdf">
+      pdf
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      pdf_url: '../../resume.pdf'
+    }
+  },
+  methods: {
+    pdf(){
+      window.open( `/pdf/web/viewer.html?file=${encodeURIComponent(this.pdf_url)}`);
+    }
+  }
+}
+</script>
