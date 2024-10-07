96c7c09cf1d73fa1f64aa0c3101e1de58a1e2d5b
nlply <yangliu.nlp@gmail.com>
2024-10-07 19:24:44 +0900

    1. change the icons of th e external links.
    2. add the link of google scholar.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,28 @@ import App from './App.vue'
 import router from './router'
 import naive from 'naive-ui'
 
+// add oh-vue-icons
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+
+import { AiGoogleScholarSquare } from "oh-vue-icons/icons";
+import { AiCvSquare } from "oh-vue-icons/icons";
+import { FaGithubSquare } from "oh-vue-icons/icons";
+import { FaTwitterSquare } from "oh-vue-icons/icons";
+
+
+
+
+
+addIcons(AiGoogleScholarSquare,AiCvSquare,FaGithubSquare,FaTwitterSquare);
+
 
 
 
 const app = createApp(App)
 app.use(router)
 app.use(naive)
+
+// add oh-vue-icons
+app.component("v-icon", OhVueIcon);
+
 app.mount('#app')
